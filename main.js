b44d2afa1bc1ab46a76257b2f7afe352db69a7b8
yw2001 <84959633+yw2001@users.noreply.github.com>
2021-10-21 16:25:10 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+console.log("My first console project");
+var first_name = "Yahvi";
+ var last_name = "Wadhwa";
+var full_name = first_name.concat(last_name);
+ console.log(full_name);
+  var a = 10;
+   var b = 5;
+    var add = a + b ;
+     console.log(add);
+      var sub = a - b ;
+       console.log(sub);
+        var multiple = a * b ;
+         console.log(multiple); 
+         var divide = a / b ;
+          console.log(divide);
